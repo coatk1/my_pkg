5a618f97fc14659f86d1e171bd82d5086825d94c
Curtis Hampton <curtishampton@Curtiss-MacBook-Pro.local>
2019-12-05 19:05:56 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+import versioneer
+
+requirements = [
+]
+
+setup(
+    name='my_pkg',
+    version=versioneer.get_version(),
+    description="My package",
+    author="Curtis Hampton",
+    author_email='CurtLHampton@gmail.com',
+    packages=['my_pkg'],
+    install_requires=requirements,
+    keywords='my_pkg',
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
