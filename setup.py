32ec1b69c011bed5846b10b3bf40523125bd2a83
CurtLH <CurtLHampton@gmail.com>
2020-04-09 01:13:40 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,17 @@ import versioneer
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="my_pkg_478",
     version=versioneer.get_version(),
     description="My package",
+    long_description=long_description,
     author="Curtis Hampton",
     author_email="CurtLHampton@gmail.com",
+    url="https://github.com/CurtLH/my_pkg",
     packages=["my_pkg"],
     install_requires=requirements,
     entry_points={"console_scripts": ["my_pkg=my_pkg.cli:my_pkg"]},
